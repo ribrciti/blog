f3b02a694e7da0c24ca07331737bd082ebee611d
ribrciti <rbciti@msn.com>
2015-10-17 12:41:52 -0700

    Added friendly_id gem to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016194212) do
+ActiveRecord::Schema.define(version: 20151017192533) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 255, null: false
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20151016194212) do
     t.string   "link",        limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.string   "slug",        limit: 255
   end
+
+  add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true, using: :btree
 
 end
